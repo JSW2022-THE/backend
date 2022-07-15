00f749c36b234654dc6798b1f763b2a8d38cb50b
roy6307 <a01024070849@gmail.com>
2022-07-16 02:39:40 +0900

    Something went wrong, but I think I fixed!

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const user = require("./user")
+const store = require("./store")
 
 /**
  * @swagger
@@ -8,5 +9,14 @@ const user = require("./user")
  *   description: 유저 추가 수정 삭제 조회
  */
 router.use("/user", user)
+
+/**
+ * @swagger
+ * tages:
+ *  name: Stores
+ *  description: 가게 추가 수정 삭제 조회
+ */
+router.use("/store", store)
+
 
 module.exports = router
