9ec46178412a3737cc1607c82cb89ac8051edd83
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2022-07-17 19:49:33 +0900

    cookie parser 설치

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port = 2000;
 const api = require("./routers")
 const { swaggerUi, specs } = require("./swagger/swagger");
 const db = require("./models");
+const cookieParser = require("cookie-parser");
 
 db.sequelize.sync(); // sequelize init
 
 //Express 4.16.0버전 부터 body-parser의 일부 기능이 익스프레스에 내장 body-parser 연결
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use("/api", api);
 
