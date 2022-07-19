6ce7dcc0abecb50d7b83505e74d720c0f022dee7
InfiniteBuffe <46419512+InfiniteBuffe@users.noreply.github.com>
2022-07-20 01:57:20 +0900

    store 테이블 생성

---
+++ migrations/20220719160720-create-store.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('stores', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      lat: {
+        type: Sequelize.DOUBLE
+      },
+      lon: {
+        type: Sequelize.DOUBLE
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      heart: {
+        type: Sequelize.BIGINT
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('stores');
+  }
+};
