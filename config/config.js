c0cc24039c4fbb112500770668a804a5c7132f47
HongRok Kim <hlog2e@gmail.com>
2022-08-02 23:04:57 +0900

    Sequelize Timezone 한국으로 설정

--- config/config.js
+++ config/config.js
@@ -1,49 +1,52 @@
-require('dotenv').config();
+require("dotenv").config();
 const env = process.env;
 
 const development = {
-    username: env.MYSQL_USERNAME,
-    password: env.MYSQL_PASSWORD,
-    database: env.MYSQL_DATABASE,
-    host: env.MYSQL_HOST,
-    dialect: "mysql",
-    port: env.MYSQL_PORT,
-    pool: {
-        max: 7,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  username: env.MYSQL_USERNAME,
+  password: env.MYSQL_PASSWORD,
+  database: env.MYSQL_DATABASE,
+  host: env.MYSQL_HOST,
+  dialect: "mysql",
+  port: env.MYSQL_PORT,
+  pool: {
+    max: 7,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  timezone: "+09:00",
 };
 
 const production = {
-    username: env.MYSQL_USERNAME,
-    password: env.MYSQL_PASSWORD,
-    database: env.MYSQL_DATABASE,
-    host: env.MYSQL_HOST,
-    dialect: "mysql",
-    port: env.MYSQL_PORT,
-    pool: {
-        max: 7,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  username: env.MYSQL_USERNAME,
+  password: env.MYSQL_PASSWORD,
+  database: env.MYSQL_DATABASE,
+  host: env.MYSQL_HOST,
+  dialect: "mysql",
+  port: env.MYSQL_PORT,
+  pool: {
+    max: 7,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  timezone: "+09:00",
 };
 
 const test = {
-    username: env.MYSQL_USERNAME,
-    password: env.MYSQL_PASSWORD,
-    database: env.MYSQL_DATABASE_TEST,
-    host: env.MYSQL_HOST,
-    dialect: "mysql",
-    port: env.MYSQL_PORT,
-    pool: {
-        max: 7,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  username: env.MYSQL_USERNAME,
+  password: env.MYSQL_PASSWORD,
+  database: env.MYSQL_DATABASE_TEST,
+  host: env.MYSQL_HOST,
+  dialect: "mysql",
+  port: env.MYSQL_PORT,
+  pool: {
+    max: 7,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  timezone: "+09:00",
 };
 
 module.exports = { development, production, test };
