919c8ced862a61882e96ee5373cdf3b1cfce63fa
HongRok Kim <hlog2e@gmail.com>
2022-08-13 21:46:05 +0900

    임시적으로 req.ip 확인하는 로직 추가

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/", (req, res) => {
   res.send("What are you looking for here?");
+  console.log(req.headers["x-forwarded-for"] || req.connection.remoteAddress);
 });
 
 server.listen(port, () => {
