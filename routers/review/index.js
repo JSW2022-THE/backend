92a63c716b97a22c98487d6e6a28c360a3e38692
HongRok Kim <hlog2e@gmail.com>
2022-08-17 04:33:36 +0900

    Review를 CREATE, GET 하는 로직 완성

--- routers/review/index.js
+++ routers/review/index.js
@@ -1,6 +1,7 @@
 const reviewRouter = require("express").Router();
 const reviewController = require("./review.controller");
 
+reviewRouter.post("/createReview", reviewController.createReview);
 reviewRouter.get("/getReviews", reviewController.getReviews);
 
 module.exports = reviewRouter;
--- routers/review/review.controller.js
+++ routers/review/review.controller.js
@@ -1,5 +1,28 @@
+const Reviews = require("../../models").Reviews;
+const uuid = require("uuid");
+
 module.exports = {
+  createReview: async (req, res) => {
+    const userUuid = req.userUuid;
+    const review_msg = req.body.review_msg;
+    const store_uuid = req.body.store_uuid;
+
+    await Reviews.create({
+      review_uuid: uuid.v4(),
+      review_msg: review_msg,
+      store_uuid: store_uuid,
+      creator_uuid: userUuid,
+    });
+
+    res.send("리뷰 작성이 완료되었습니다.");
+  },
   getReviews: async (req, res) => {
-    res.send("good review");
+    const store_uuid = req.query.store_uuid;
+
+    const reviews = await Reviews.findAll({
+      where: { store_uuid: store_uuid },
+    });
+
+    res.json(reviews);
   },
 };
